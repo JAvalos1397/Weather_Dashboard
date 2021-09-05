c9d29602fcbe9a22db0566e411f4c20e6146541b
Jose Avalos <avaljose@hotmai.com>
2021-09-05 13:00:07 -0500

    Finished Current weather, and clear key

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,30 @@
-const cityElement = $("#cityName");
-const temp = $("#temp");
-const wind = $('#wind');
-const uv = $('#uv');
+const cityContainer = $("#cityName");
+const tempContainer = $("#temp");
+const windContainer = $('#wind');
+const uvContainer = $('#uv');
 const apiKey = "5cd2b271a245b10cee40362f079a84fd";
 
-function getWeather(searchResults){
+function getWeather(userInput){
 
-    const getapiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchResults}&units=imperial&appid=${apiKey}`
+    const getapiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=${apiKey}`
     fetch(getapiUrl)
     .then (response => response.json())
     .then(data => {
         console.log(data)
         const temp = data.main.temp;
-        const weather = data.weather[0].icon;
+        const icon = data.weather[0].icon;
         const wind = data.wind.speed;
         const lon = data.coord.lon;
         const lat = data.coord.lat;
-        let UVQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}`;
+        const name = data.name;
+        let UVQueryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${apiKey}`;
         fetch(UVQueryURL)
         .then(response => response.json())
         .then(data => {
            console.log(data)
-        const uv = data.current.uvi
-        console.log(uv)
-        renderWeather(temp,weather,wind,uv)
+        const uv = data.current.uvi  
+         
+        renderWeather(name,temp,icon,wind,uv)
     })
     });
 }
@@ -31,16 +32,63 @@ function getWeather(searchResults){
 $("#submit").on('click', function(event) {
  event.preventDefault(event);
 
- const searchResults = $("#city").val();
-    console.log(searchResults)
- getWeather(searchResults)
- searchHistory(searchResults)
+ const userInput = $("#city").val();
+ getWeather(userInput);
+ searchHistory(userInput);
+ $(".empty").empty();
+
+
 })
 
-function renderWeather(temp,weather,wind,uv){
+function renderWeather(name,temp,icon,wind,uv){
+    if (uv >= 6){
+        var severity = 'high'
+    } else if (uv <= 2){
+        var severity = 'low'
+    } else{
+        var severity = 'moderate'
+    }  
+console.log(name,temp,icon,wind,uv)
+    const title = $('<h2>');
+    const cardT = $('<p>');
+    const cardS = $('<img>');
+    const cardWS = $('<p>');
+    const cardUV = $('<p>');
+    const span = $('<span>')
+//adding weather
+    title.text(name);
+    title.addClass("center empty")
+    cardS.attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`);
 
+
+    cardT.text(`Temperature: ${temp}`)
+    cardT.addClass('fw-bold empty')
+
+    cardWS.text(`WindSpeed: ${wind}`)
+    cardWS.addClass('fw-bold empty')
+
+    cardUV.text('UV: ')
+    cardUV.addClass("fw-bold empty")
+
+    span.addClass(severity)
+    span.text(`${uv}`)
+    span.addClass("fw-bold empty")
+
+
+//appending weather
+    cityContainer.append(title)
+    cityContainer.append(cardS)
+    tempContainer.append(cardT)
+    
+    windContainer.append(cardWS)
+    uvContainer.append(cardUV)
+    uvContainer.append(span)
 }
 
 function searchHistory(response){
+
+}
+
+function clear(){
 
 }
