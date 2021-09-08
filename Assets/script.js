92b7ab1417251a41a68aba0fa376f6ab1871f4aa
Jose Avalos <avaljose@hotmai.com>
2021-09-07 23:11:19 -0500

    added local storage set and get

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,8 @@ const cityContainer = $("#cityName");
 const tempContainer = $("#temp");
 const windContainer = $('#wind');
 const uvContainer = $('#uv');
+const historyContainer = $('#history')
+const currentContainer = $('#currentDay')
 const apiKey = "5cd2b271a245b10cee40362f079a84fd";
 
 function getWeather(userInput){
@@ -21,32 +23,51 @@ function getWeather(userInput){
         fetch(UVQueryURL)
         .then(response => response.json())
         .then(data => {
-           console.log(data)
+        
         const uv = data.current.uvi  
          
         renderWeather(name,temp,icon,wind,uv)
+        const forecast = data.daily;
+        forecast.forEach(element,i) => {
+                
+        renderForecast(forecast[i]);
+
+        }
     })
     });
 }
 
+//sending search value
 $("#submit").on('click', function(event) {
  event.preventDefault(event);
-
+ 
  const userInput = $("#city").val();
+ $("#city").val('')
  getWeather(userInput);
- searchHistory(userInput);
+ setHistory(userInput);
  $(".empty").empty();
-
-
 })
 
+//sending value of history
+$(document).on('click','.card-body',function() {
+    const history = $(this).text();
+    console.log(history)
+    getWeather(history);
+    $(".empty").empty();
+})
+
+function renderForecast(data){
+    console.log(data)
+}
+
+//renderCurrent weather 
 function renderWeather(name,temp,icon,wind,uv){
     if (uv >= 6){
-        var severity = 'high'
+        var severity = 'bg-danger'
     } else if (uv <= 2){
-        var severity = 'low'
+        var severity = 'bg-success'
     } else{
-        var severity = 'moderate'
+        var severity = 'bg-warning'
     }  
 console.log(name,temp,icon,wind,uv)
     const title = $('<h2>');
@@ -57,22 +78,23 @@ console.log(name,temp,icon,wind,uv)
     const span = $('<span>')
 //adding weather
     title.text(name);
-    title.addClass("center empty")
+    title.addClass("center")
     cardS.attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`);
+    cityContainer.addClass('bg-info')
 
 
     cardT.text(`Temperature: ${temp}`)
-    cardT.addClass('fw-bold empty')
+    cardT.addClass('fw-bold')
 
     cardWS.text(`WindSpeed: ${wind}`)
-    cardWS.addClass('fw-bold empty')
+    cardWS.addClass('fw-bold')
 
     cardUV.text('UV: ')
-    cardUV.addClass("fw-bold empty")
+    cardUV.addClass("fw-bold")
 
     span.addClass(severity)
     span.text(`${uv}`)
-    span.addClass("fw-bold empty")
+    span.addClass("fw-bold")
 
 
 //appending weather
@@ -85,10 +107,53 @@ console.log(name,temp,icon,wind,uv)
     uvContainer.append(span)
 }
 
-function searchHistory(response){
-
+function generatehistory(response){
+    const btn = $('<button>')
+    const btnClass = 'btn btn-outline-dark card-body'
+    
+    btn.attr('type','button')
+    btn.addClass(btnClass)
+    btn.text(response)
+    historyContainer.append(btn)
 }
 
-function clear(){
 
+//clearing History/ and storage
+$('#clear').on('click', (event) => {
+    $("#history").empty();
+    localStorage.clear();
+})
+
+// SAVE then get
+
+ function setHistory(response) {
+    var cities = [];
+    var uniqueCity = [];
+    
+    var getCity = response;
+    
+    cities = JSON.parse(localStorage.getItem('SearchHistory')) || [];
+    cities.push(getCity);
+
+    $.each(cities, function(i, el){
+        if($.inArray(el, uniqueCity) === -1) uniqueCity.push(el);
+    });
+    localStorage.setItem("SearchHistory", JSON.stringify(uniqueCity)); 
+    getHistory()   
 }
+
+function getHistory () {
+    var retrieveData = localStorage.getItem("SearchHistory") 
+    var historyList = JSON.parse(retrieveData)
+    // console.log(parkCode)
+    $("#history").empty();
+
+    for (let i = 0; i < historyList.length; i++) {
+        var city = historyList[i];
+        if (city )
+        console.log(city);
+        generatehistory(city);
+    }
+}
+
+getHistory()
